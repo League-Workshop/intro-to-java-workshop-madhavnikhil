06617f3aad3f9189468d8f9bcea05b97673a5579
root <root@de8e48e4992b>
2020-04-10 21:47:24 +0000

    Maze

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -16,17 +16,17 @@ public class QuizGame {
 	    if(input.equalsIgnoreCase("15")) {
 	    		System.out.println("answer is correct");
 	    		// 4.  if the user's answer was correct, add one to their score 
-	    		score = score + 1;
+	    		score = score + 3;
 	    }
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		JOptionPane.showInputDialog("Say Nikhil, Tyler, Gavin, and Igor are going to Disney World and all of us get a 4 day ticket with 1 park per day and it costs $436 per person, then how much money does it cost all 4 of us?");
-		if(input.equalsIgnoreCase("1744")) {
+		String input2 = JOptionPane.showInputDialog("Say Nikhil, Tyler, Gavin, and Igor are going to Disney World and all of us get a 4 day ticket with 1 park per day and it costs $436 per person, then how much money does it cost all 4 of us?");
+		if(input2.equalsIgnoreCase("1744")) {
 			System.out.println("answer is correct");
-			score = score + 1
+			score = score + 3;
 			}
 		{
-			JOptionPane.showInputDialog("Now if the whole trip cost is $5,691 knowing that the cost of the ticket is $1744 and let the cost of the hotel be x and the flight be y, then find the value of x and y considering that y is 55 more than x?");
-			
+		String input3 = JOptionPane.showInputDialog("Now if the whole trip cost is $5,691 knowing that the cost of the ticket is $1744 and let the cost of the hotel be x and the flight be y, then find the value of x and y considering that y is 55 more than x?");
+			if(input3.equalsIgnoreCase()
 			
 			
 		}
